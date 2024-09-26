ac033f6c7a143c9abd209de5ea28d59aeb55e424
Tamjidthecoder <tamjidcool797@gmail.com>
2024-09-26 23:15:08 +0600

    Add files via upload

---
+++ hw.py
@@ -0,0 +1,10 @@
+
+def circle_circumference(radius):
+    return 2  
+
+def rectangle_perimeter(length, width):
+    return 2 * (length + width)
+
+def triangle_perimeter(side1, side2, side3):
+    return side1 + side2 + side3
+print(triangle_perimeter)
